bb0965743dcde7afc8163ccbcaa06ab47bbca5e5
Ahmed Kandil <ahmedkandil@Ahmeds-MacBook-Air.local>
2023-12-26 11:44:42 +0200

    Add some content the Home page

--- Home.py
+++ Home.py
@@ -1,13 +1,29 @@
 import streamlit as st
 
-st.set_page_config("CCS Project", page_icon="🧮")
-st.title("CCS Project")
 
-st.markdown(
+st.set_page_config("Computational Cognitive Science Project", page_icon="🧮")
+
+st.title("Computational Cognitive Science Project")
+st.write(
+    "Welcome to the Computational Cognitive Science project! This project focuses on implementing various Naive Bayes classifiers "
+    "for solving different equations in the field of Natural Language Processing (NLP) and more."
+)
+
+st.header("Equations:")
+st.write(
     """
-    ## Personal Details
-    - **Name:** Ahmed Amro Abd-Elmoneim Moursi Kandil
-    - **ID:** 4211133
-    - **Group:** A1
-    """
+    - Exponential Naive Bayes Classifier
+    - NLP Naive Bayes Classifier
+    - Gaussian Naive Bayes Classifier
+    - Naive Bayes Classifier
+    - Mix Naive Bayes Classifier
+    - Posterior Probabilities
+"""
+)
+
+st.header("Thanks")
+st.write(
+    "A special thanks to :red[***Dr.Sayed***] for guidance and :blue[***Delta University For Science And Technology***] "
+    "for providing the platform for this project. "
+    "We appreciate the support and resources that have made this research possible."
 )
