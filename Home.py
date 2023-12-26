7359fad09d4886c8950f7d9e7c61fee76865a762
Ahmed Kandil <ahmedkandil@Ahmeds-MacBook-Air.local>
2023-12-26 20:27:29 +0200

    Update Home page

--- Home.py
+++ Home.py
@@ -1,15 +1,17 @@
 import streamlit as st
 
 
-st.set_page_config("Computational Cognitive Science Project", page_icon="🧮")
+st.set_page_config(page_title="Computational Cognitive Science Project", page_icon="🧮")
 
 st.title("Computational Cognitive Science Project")
+
 st.write(
-    "Welcome to the Computational Cognitive Science project! This project focuses on implementing various Naive Bayes classifiers "
-    "for solving different equations in the field of Natural Language Processing (NLP) and more."
+    "Welcome to the Computational Cognitive Science project! This endeavor is dedicated to the implementation "
+    "of advanced Naive Bayes classifiers. These classifiers are designed to tackle diverse equations within "
+    "Natural Language Processing (NLP) and beyond."
 )
 
-st.header("Equations:")
+st.header("📊 Equations")
 st.write(
     """
     - Exponential Naive Bayes Classifier
@@ -21,9 +23,10 @@ st.write(
 """
 )
 
-st.header("Thanks")
+st.header("🌹 Acknowledgments")
 st.write(
-    "A special thanks to :red[***Dr. Sayed***] for guidance and :blue[***Delta University For Science And Technology***] "
-    "for providing the platform for this project. "
-    "We appreciate the support and resources that have made this research possible."
+    "I sincerely thank :red[***Dr. Sayed Hussein Haggag***] for invaluable guidance throughout the semester. Special thanks to "
+    ":blue[***Delta University For Science And Technology***] for providing the environment that facilitated the "
+    "realization of this research. I deeply appreciate the unwavering support and resources that have contributed "
+    "to the success of this project."
 )
